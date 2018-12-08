ed3a78b6e4b4037e4d5f35e8677d1e7ad0dda4ec
TooFiveFive <e.moynihan00@gmail.com>
2018-12-08 17:36:05 +0000

    added base

---
+++ README.md
@@ -0,0 +1,3 @@
+# py-the-way
+
+A recommendation system for suggesting where a user should live within the USA based on input data and datasets
---
+++ csv_json.py
@@ -0,0 +1,29 @@
+import json, sys
+
+if (len(sys.argv) != 2):
+    print("You need to have one arg")
+    exit()
+
+# IMPORTANT: make sure that the only commas are used for column separation!
+
+with open(sys.argv[1]) as file:
+
+    file = [s.replace("\n", "") for s in file]
+    file = [s.replace("\"", "") for s in file]
+
+    keys = file[0].split(",")
+    dataL = []
+
+    for l in file[1:]:
+        l = l.split(",")
+        dl = dict((dd, "") for dd in keys)
+        for i,k in enumerate(keys):
+            dl[k] = l[i]
+        dataL.append(dl)
+
+    dataJson = json.dumps(dataL, indent=4)
+
+    write = open(f"{sys.argv[1][:-4]}.json", "w")
+
+    write.write(dataJson)
+
