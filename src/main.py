f217ecf5f6b577d0c4394bc366bc9109a80b7fa6
TooFiveFive <mail@er1c.me>
2018-12-12 00:48:30 +0000

    iterater

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from recommender import Residential, Recommender
 
-test_data = Residential(1,2,3,4)
+test_data = Residential(1,2,3,4,6,5)
+
+test_data.set_prefs(10000,200000,True,"tech")
 
 r = Recommender("residential",test_data)
-
--- src/recommender/__init__.py
+++ src/recommender/__init__.py
@@ -11,7 +11,7 @@ class Recommender:
 
 class Residential:
     # will be equal to an order index
-    def __init__(self, education, jobs, hapiness, weather):
+    def __init__(self, education, jobs, hapiness, weather, health, wealth):
 
         frame = inspect.currentframe()
         args,_,_,vals = inspect.getargvalues(frame)
@@ -19,8 +19,28 @@ class Residential:
         for v in args[1:]:
             self.__dict__[vals[v]] = v
 
+    def set_prefs(self, pop, price, urban, industry):
+
+        frame = inspect.currentframe()
+        args,_,_,vals = inspect.getargvalues(frame)
+
+        self.prefs = {}
+
+        for v in args[1:]:
+            self.prefs[v] = vals[v]
+
     def __getitem__(self,key):
         return self.__dict__[key]
+    
+    def is_int(self, i):
+        try:
+            int(i)
+            return True
+        except:
+            return False
+
+    def keys(self):
+        return [k for k in self.__dict__.keys() if self.is_int(k)]
 
     def __str__(self):
         return repr(self.__dict__)
