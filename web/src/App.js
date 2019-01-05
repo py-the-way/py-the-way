6b8af548f91d16f5ac9c9193c161077022bbf56d
Thomas Galligan <thomas007galligan@gmail.com>
2019-01-05 20:56:10 +0000

    added switch cases

--- web/src/App.js
+++ web/src/App.js
@@ -54,57 +54,61 @@ class App extends Component {
     .then(result=>result.json()).then(items=>{this.setState({items, submit:"yes"}); console.log(items)});
   }
   render() {
-    if (this.state.submit === "no") {
-      return (
-        <div className="App">
-          <div className={"view"}>
-            <h2>Py The Way</h2>
-            <p>A recommendation system for suggesting where a user should live within the USA based on input data and datasets</p>
+    switch(this.state.submit){
+      default: {
+        return null
+      }
+      case "no": {
+        return (
+          <div className="App">
+            <div className={"view"}>
+              <h2>Py The Way</h2>
+              <p>A recommendation system for suggesting where a user should live within the USA based on input data and datasets</p>
+            </div>
+            <h3>Get your recommendations</h3>
+            <p>First of all, below are some of the things people look for when moving to a new area. Drag them in order of your personal prefence:</p>
+            <Reorder
+              reorderId="myList" component="ul"  className={"order"} draggedClassName={"dragged"} lock="horizontal" holdTime={0} onReorder={this.onReorder.bind(this)}
+              
+            >
+              {
+                this.state.list.map((val, i) => (
+                  <div key={val} className={"sel"}>
+                    <li
+                      className={"listItem"}
+                    >
+                      {i+1}: {val}
+                    </li>
+                  </div>
+                )).toArray()
+              }
+            </Reorder>
+            <br />
+            <p>Now, lets get an estimate on some of the figures you'd like to see from your new home:</p>
+            <div className={"dragger"}>
+              <span>Population of county:</span>
+              <Slider onChange={this.popSlide} className={"drag"} marks={{ 1000: '1,000', 10000000: '10,000,000' }} min={1000} max={10000000}  handle={this.handle} />
+            </div>
+            <br />
+            <div className={"dragger"}>
+              <span>Price of a home ($):</span>
+              <Slider onChange={this.prSlide} className={"drag"} marks={{ 10000: '10,000', 5000000: '5,000,000' }} min={10000} max={5000000} handle={this.handle} />
+            </div>
+            <br />
+            <div className={"dragger"}>
+              <span>Percentage Urban:</span>
+              <Slider onChange={this.urSlide} className={"drag"} marks={{ 0: '0%', 100: '100%' }} min={0} max={100} handle={this.handle} />
+            </div>
+            <button onClick={this.submit}>Get yours</button>
           </div>
-          <h3>Get your recommendations</h3>
-          <p>First of all, below are some of the things people look for when moving to a new area. Drag them in order of your personal prefence:</p>
-          <Reorder
-            reorderId="myList" component="ul"  className={"order"} draggedClassName={"dragged"} lock="horizontal" holdTime={0} onReorder={this.onReorder.bind(this)}
-            
-          >
-            {
-              this.state.list.map((val, i) => (
-                <div key={val} className={"sel"}>
-                  <li
-                    className={"listItem"}
-                  >
-                    {i+1}: {val}
-                  </li>
-                </div>
-              )).toArray()
-            }
-          </Reorder>
-          <br />
-          <p>Now, lets get an estimate on some of the figures you'd like to see from your new home:</p>
-          <div className={"dragger"}>
-            <span>Population of county:</span>
-            <Slider onChange={this.popSlide} className={"drag"} marks={{ 1000: '1,000', 10000000: '10,000,000' }} min={1000} max={10000000}  handle={this.handle} />
-          </div>
-          <br />
-          <div className={"dragger"}>
-            <span>Price of a home ($):</span>
-            <Slider onChange={this.prSlide} className={"drag"} marks={{ 10000: '10,000', 5000000: '5,000,000' }} min={10000} max={5000000} handle={this.handle} />
-          </div>
-          <br />
-          <div className={"dragger"}>
-            <span>Percentage Urban:</span>
-            <Slider onChange={this.urSlide} className={"drag"} marks={{ 0: '0%', 100: '100%' }} min={0} max={100} handle={this.handle} />
-          </div>
-          <button onClick={this.submit}>Get yours</button>
-        </div>
-      );
-    }
-    if (this.state.submit === "ing") {
+        );
+      }
+      case "ing":{
       return (
         <div className={"load"}><h5>Loading your data<svg width="78px"  height="78px"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="lds-liquid"><circle cx="50" cy="50" ng-attr-r="{{config.radius}}" ng-attr-stroke="{{config.c1}}" ng-attr-stroke-width="{{config.width}}" fill="none" r="35" stroke="#b82651" stroke-width="6"></circle><path ng-attr-d="{{config.d}}" ng-attr-fill="{{config.c2}}" d="M 21 50 Q 35.5 61.901 50 50 Q 64.5 38.099 79 50 A 29 29 0 0 1 21 50" fill="#b82651"><animate attributeName="d" calcMode="spline" values="M21 50 Q35.5 37 50 50 Q64.5 63 79 50 A29 29 0 0 1 21 50;M21 50 Q35.5 63 50 50 Q64.5 37 79 50 A29 29 0 0 1 21 50;M21 50 Q35.5 37 50 50 Q64.5 63 79 50 A29 29 0 0 1 21 50" keyTimes="0;0.5;1" dur="1" keySplines="0.5 0 0.5 1;0.5 0 0.5 1" begin="0s" repeatCount="indefinite"></animate></path></svg></h5></div>
       )
     }
-    if (this.state.submit === "yes") {
+    case "yes": {
       return (
         <div><h3>Your recommendations</h3>
         <table><tbody>
@@ -118,7 +122,8 @@ class App extends Component {
             <tr><th>{i+1}.</th><td><a title={"Google Maps"} target={"_blank"} href={"https://www.google.com/maps/search/"+x.geo_name}>{x.geo_name}</a></td><td>{x.info_pop.toLocaleString()}</td><td>&euro;{x.info_price.toLocaleString()}</td></tr>
           )}
         </tbody></table><a href={"/"}><button>Do it again</button></a></div>
-      )
+        )
+      }
     }
   }
 }
